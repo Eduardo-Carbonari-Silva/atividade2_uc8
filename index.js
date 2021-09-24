63b66b6e47d983d00fda4f40a26ce4758108c074
Eduardo-Carbonari-Silva <89611865+Eduardo-Carbonari-Silva@users.noreply.github.com>
2021-09-24 15:41:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,29 +3,39 @@ var nome;
 var dataDoEvento;
 var idade;
 var dataAtual;
-var participantes = 50;
+var participantes = 0;
 
 nome = readlineSync.question("Digite seu nome: ");
 dataDoEvento = readlineSync.question("Digite a data do evento: ");
 idade = readlineSync.question("Digite sua idade: ");
 dataAtual = readlineSync.question("Digite a data atual: ");
 
-if(dataDoEvento > dataAtual){
-	//condição verdadeira
-	if(idade > 18){
+while (participantes < 100) {
+
+	if(dataDoEvento > dataAtual){
 		//condição verdadeira
-		if(participantes < 100){
+		if(idade > 18){
 			//condição verdadeira
-			console.log("Participante cadastrado com sucesso!");
+			if(participantes < 100){
+				//condição verdadeira
+				console.log("Participante cadastrado com sucesso!");
+			}else{
+				//condição falsa
+				console.log("Cadastro não permitido, lista de participantes excedida!");
+			}
 		}else{
 			//condição falsa
-			console.log("Cadastro não permitido, lista de participantes excedida!");
+			console.log("Cadastro não permitido, idade inválida!");
 		}
 	}else{
 		//condição falsa
-		console.log("Cadastro não permitido, idade inválida!");
+		console.log("Cadastro não permitido, data inválida!");
 	}
-}else{
-	//condição falsa
-	console.log("Cadastro não permitido, data inválida!");
+
+	nome = readlineSync.question("Digite seu nome: ");
+	dataDoEvento = readlineSync.question("Digite a data do evento: ");
+	idade = readlineSync.question("Digite sua idade: ");
+	dataAtual = readlineSync.question("Digite a data atual: ");
+	participantes++;
 }
+console.log("Limite de cadastros atingido!");
